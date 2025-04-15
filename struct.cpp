8b535a28c9541930f3d8e8b0f9fb79bdb7709867
nisrinahnfhh <nisrinahnfhh@gmail.com>
2025-04-15 15:21:18 +0700

    masukkan data mahasiswa

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,19 @@ struct mahasiswa{
     string alamat;
 };
 
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20240140001";
+    mhs.nama = "Abra";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat = ";
+    cin >> mhs.alamat;
+    
+
+}
