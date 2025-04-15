bcb2290785d35cb33d51e274f70130b58177c585
nisrinahnfhh <nisrinahnfhh@gmail.com>
2025-04-15 15:04:39 +0700

    mengisi array

--- array.cpp
+++ array.cpp
@@ -8,3 +8,14 @@ int skor[5] = {8, 12, 18, 6};
 //deklarasi array dengan ukuran dan tidak ada nilai awal
 string nama[3];
 
+int main(){
+    cout << "Skor ke 3 = " << skor[2] << endl;
+    skor[2] = 25;
+    cout << "Skopr ke 3 = " << skor [2] << endl;
+
+    //mengisi array dengan loop for 
+    for(int i = 0; i < 3; i++){
+        cout << "Nama Mahasiswa ke-" << i+1 << " = ";
+        cin >> nama[i];
+    }
+}
