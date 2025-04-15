16098f2dd86d465447488a8ad03ad3f9ab756222
nisrinahnfhh <nisrinahnfhh@gmail.com>
2025-04-15 16:00:21 +0700

    struct4

--- struct4.cpp
+++ struct4.cpp
@@ -12,26 +12,23 @@ struct mahasiswa{
     string nama;
     detailAlamat alamat;
 };
-
 int main(){
-    mahasiswa mhs;
+    mahasiswa mhs[2];
 
-    cout << "Masukkan NIM = ";
-    cin >> mhs.nim;
+    for(int i = 0; i < 2; i++){
+        cout << "Mahasiswa ke- " << i+1 << endl;
+        cout << "Masukkan NIM = ";
+    cin >> mhs[i].nim;
     cin.ignore();
     cout << "Masukkan Nama = ";
     cin >> mhs.nama;
-    getline(cin, mhs.nama);
+    getline(cin, mhs[i].nama);
     cout << "Masukkan Alamat Desa = ";
-    cin >> mhs.alamat.desa;
+    cin >> mhs[i].alamat.desa;
     cout << "Masukkan Alamat Kota = ";
-    cin >> mhs.alamat.kota;
+    cin >> mhs[i].alamat.kota;
 
     cout << endl;
     cout << "Data Mahasiswa" << endl;
-
-    cout << "NIM = " << mhs.nim << endl;
-    cout << "Nama = " << mhs.nama << endl;
-    cout << "Alamat Desa = " << mhs.alamat.desa << endl;
-    cout << "Alamat Kota = " << mhs.alamat.kota << endl;
-}
+    
+    }
